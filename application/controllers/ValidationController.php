3d907f0f505e35bcda5ee9bdff0a7a28bb50a7e4
phich82 <phich82@gmail.com>
2019-01-23 01:30:03 +0700

    [huynh phat] Update code

--- application/controllers/ValidationController.php
+++ application/controllers/ValidationController.php
@@ -20,4 +20,38 @@ class ValidationController extends CI_Controller
 
         return $this->load->view('validation/index');
     }
+
+    public function indexPost()
+    {
+        // validate params from request
+        $validator = new TestValidationRequest();
+        //var_dump($validator->hasError());
+
+        $this->lang->load('form_validation_lang');
+
+        return $this->load->view('validation/index');
+    }
+
+    /**
+     * @override
+     * _remap
+     *
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     */
+    public function _remap($method, $args = []) {
+        // remove suffix of method if any
+        $methodNoSuffix = preg_replace('/(.*)(post|get|put|delete|head|options)/i', '$1', $method);
+        // append the new suffix to it
+        if (strlen($methodNoSuffix) !== strlen($method)) {
+            $methodNoSuffix = $method.ucfirst($this->input->method());
+        }
+        if (method_exists($this, $method)) {
+            // validate security here
+
+            return call_user_func_array([$this, $method], $args);
+        }
+        throw new Exception('Not Method Allowed');
+    }
 }
